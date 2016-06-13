25b2e0a32e3831341afde62c9a9aeeac606373fc
sakura-pc <841105127@qq.com>
2016-06-13 12:12:45 +0800

    v0.2

--- v0.2/js/main.js
+++ v0.2/js/main.js
@@ -1,8 +1,10 @@
 require(['jquery','window'],function($,w){
     $('#a').click(function(){
-        new w.Window().alert('Hello World!',function(){
-            alert('你点击了关闭按钮')
-        },{
+        new w.Window().alert({
+            content:'Hello World!',
+            handler:function(){
+                alert('你点击了关闭按钮');
+            },
             width:300,
             height:150,
             y:50
