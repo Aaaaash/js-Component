ef448e4b43340e6b9eeca1947ab656cf04062656
sakura-pc <841105127@qq.com>
2016-06-13 15:07:55 +0800

    增加了皮肤定制接口

---
+++ v0.4/js/window.js
@@ -0,0 +1,54 @@
+define(['jquery'],function(){
+    function Window(){
+        this.cfg={
+            width:500,
+            height:300,
+            title:"系统消息",
+            content:"",
+            handler4AlertBtn:null,
+            hasCloseBtn:false,
+            skinClassName:null,
+            handler4CloseBtn:null
+        }
+    };
+    Window.prototype={
+        alert:function(cfg){
+                var CFG=$.extend(this.cfg,cfg);
+                var boundingBox=$(
+                    '<div class="window_boundingBox">'+
+                        '<div class="window_header">'+CFG.title+'</div>'+
+                        '<div class="window_body">'+CFG.content+'</div>'+
+                        '<div class="window_footer"><input type="button" value="确定" class="window_alertBtn"/></div>'+
+                    '</div>'
+                );
+                var btn=boundingBox.find('.window_alertBtn');
+                boundingBox.appendTo('body');
+                btn.click(function(){
+                    CFG.handler4AlertBtn && CFG.handler4AlertBtn();
+                    boundingBox.remove();
+                });
+                boundingBox.css({
+                    width:CFG.width+'px',
+                    height:CFG.height+'px',
+                    left:(CFG.x||(window.innerWidth-CFG.width)/2)+'px',
+                    top:(CFG.y||(window.innerHeight-CFG.height)/2)+'px'
+                });
+                if(CFG.hasCloseBtn){
+                    var closeBtn=$('<span class="window_closeBtn"></span>');
+                    closeBtn.appendTo(boundingBox);
+                    closeBtn.click(function(){
+                        CFG.handler4CloseBtn && CFG.handler4CloseBtn();
+                        boundingBox.remove();
+                    });
+                };
+                if(CFG.skinClassName){
+                    boundingBox.addClass(CFG.skinClassName);
+                }
+        },
+        confim:function(){},
+        prompt:function(){}
+    }
+    return {
+        Window:Window
+    }
+})
