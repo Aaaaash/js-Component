0aaa7c5e17010de70a6cc8b0e8ed2605c3ed6f13
sakura-pc <841105127@qq.com>
2016-06-13 11:36:39 +0800

    v0.1

--- v0.1/js/main.js
+++ v0.1/js/main.js
@@ -1,5 +1,7 @@
 require(['jquery','window'],function($,w){
     $('#a').click(function(){
-        new w.Window().alert('Hello World!');
+        new w.Window().alert('Hello World!',function(){
+            alert('你点击了关闭按钮')
+        });
     });
 });
--- v0.1/js/window.js
+++ v0.1/js/window.js
@@ -1,10 +1,16 @@
 define(['jquery'],function(){
     function Window(){};
     Window.prototype={
-        alert:function(content){
+        alert:function(content,handler){
             var boundingBox=$('<div class="window_boundingBox"></div>');
             boundingBox.appendTo('body');
             boundingBox.html('<h1>'+content+'</h1>');
+            var btn=$('<input type="button" value="关闭"/>');
+            btn.appendTo(boundingBox);
+            btn.click(function(){
+                handler&&handler();
+                boundingBox.remove();
+            })
         },
         confim:function(){},
         prompt:function(){}
