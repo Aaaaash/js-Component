87c82015aa90cb0ba4f0b1e9172187368a0e053b
sakura-pc <841105127@qq.com>
2016-06-13 14:43:41 +0800

    添加了右上角关闭按钮，并提供关闭回调函数接口

--- v0.3/js/main.js
+++ v0.3/js/main.js
@@ -3,12 +3,16 @@ require(['jquery','window'],function($,w){
         new w.Window().alert({
             title:'提示',
             content:'Hello World!',
-            handler:function(){
+            handler4AlertBtn:function(){
                 alert('你点击了关闭按钮');
+            },
+            handler4CloseBtn:function(){
+                alert('你真的点了关闭按钮')
             },
             width:400,
             height:250,
-            y:50
+            y:50,
+            hasCloseBtn:true
         });
     });
 });
--- v0.3/js/window.js
+++ v0.3/js/window.js
@@ -5,7 +5,9 @@ define(['jquery'],function(){
             height:300,
             title:"系统消息",
             content:"",
-            handler:null
+            handler4AlertBtn:null,
+            hasCloseBtn:false,
+            handler4CloseBtn:null
         }
     };
     Window.prototype={
@@ -15,13 +17,13 @@ define(['jquery'],function(){
                     '<div class="window_boundingBox">'+
                         '<div class="window_header">'+CFG.title+'</div>'+
                         '<div class="window_body">'+CFG.content+'</div>'+
-                        '<div class="window_footer"><input type="button" value="关闭"/></div>'+
+                        '<div class="window_footer"><input type="button" value="确定" class="window_alertBtn"/></div>'+
                     '</div>'
                 );
-                var btn=boundingBox.find('.window_footer input');
+                var btn=boundingBox.find('.window_alertBtn');
                 boundingBox.appendTo('body');
                 btn.click(function(){
-                    CFG.handler && CFG.handler();
+                    CFG.handler4AlertBtn && CFG.handler4AlertBtn();
                     boundingBox.remove();
                 });
                 boundingBox.css({
@@ -30,6 +32,14 @@ define(['jquery'],function(){
                     left:(CFG.x||(window.innerWidth-CFG.width)/2)+'px',
                     top:(CFG.y||(window.innerHeight-CFG.height)/2)+'px'
                 });
+                if(CFG.hasCloseBtn){
+                    var closeBtn=$('<span class="window_closeBtn"></span>');
+                    closeBtn.appendTo(boundingBox);
+                    closeBtn.click(function(){
+                        CFG.handler4CloseBtn && CFG.handler4CloseBtn();
+                        boundingBox.remove();
+                    });
+                }
         },
         confim:function(){},
         prompt:function(){}
