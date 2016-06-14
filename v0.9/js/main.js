433551691e9d762f839eb216281d6fc6eae501e8
sakura-pc <841105127@qq.com>
2016-06-14 13:56:38 +0800

    增加confirm弹出框

---
+++ v0.9/js/main.js
@@ -0,0 +1,36 @@
+require(['jquery','window'],function($,w){
+    $('#a').click(function(){
+        var win=new w.Window();
+        // win.alert({
+        //     title:'提示',
+        //     content:'Hello World!',
+        //     width:400,
+        //     height:250,
+        //     y:50,
+        //     hasCloseBtn:true,
+        //     skinClassName:"window_skin_a",
+        //     text4AlertBtn:"ok",
+        //     dragHandle:'.window_header'
+        // }).on('alert',function(){
+        //     alert('123');
+        // }).on('close',function(){
+        //     alert('321');
+        // });
+        win.confirm({
+            title:"系统消息",
+            content:"您确定要删除这个文件吗？",
+            width:300,
+            height:150,
+            y:50,
+            text4ConfirmBtn:"是",
+            text4CancelBtn:"否",
+            dragHandle:".window_header",
+            // hasCloseBtn:true
+        }).on("confirm",function(){
+            alert("确定");
+        }).on('cancel',function(){
+            alert("取消");
+        });
+    });
+
+});
