b415b42d2bff2f48f0a23936691ffc6436b1fdfa
sakura-pc <841105127@qq.com>
2016-06-14 09:11:32 +0800

    抽象出自定义事件为widget类

---
+++ v0.8/js/main.js
@@ -0,0 +1,21 @@
+require(['jquery','jqueryUI','window'],function($,$UI,w){
+    $('#a').click(function(){
+        var win=new w.Window();
+        win.alert({
+            title:'提示',
+            content:'Hello World!',
+            width:400,
+            height:250,
+            y:50,
+            hasCloseBtn:true,
+            skinClassName:"window_skin_a",
+            text4AlertBtn:"ok",
+            dragHandle:'.window_header'
+        }).on('alert',function(){
+            alert('123');
+        }).on('close',function(){
+            alert('321');
+        });
+    });
+
+});
